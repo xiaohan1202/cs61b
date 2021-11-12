87c850ef2107f2ead8449490c52e958bc0102dc7
Xiaohan <962479661@qq.com>
2021-11-12 10:54:23 +0800

    lab2

--- skeleton-sp18-master/lab2/Intlist/IntList.java
+++ skeleton-sp18-master/lab2/Intlist/IntList.java
@@ -81,8 +81,12 @@ public class IntList {
      */
 
     public static IntList dcatenate(IntList A, IntList B) {
-        //TODO:  fill in method
-        return null;
+        IntList p;
+        for(p = A; p.rest != null; p = p.rest){
+            continue;
+        }
+        p.rest = B;
+        return A;
     }
 
     /**
@@ -90,24 +94,16 @@ public class IntList {
      * * elements of B.  May NOT modify items of A.  Use 'new'.
      */
     public static IntList catenate(IntList A, IntList B) {
+        IntList st = new IntList(A.first, null);
+        IntList p;
+        A = A.rest;
+        for(p = st; A.rest != null; A = A.rest, p = p.rest){
+            p.rest = new IntList(A.first, null);
+        }
+        p.rest = new IntList(A.first, B);
         //TODO:  fill in method
-        return null;
+        return st;
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     /**
      * DO NOT MODIFY ANYTHING BELOW THIS LINE! Many of the concepts below here
@@ -231,4 +227,5 @@ public class IntList {
         return out.toString();
     }
 }
+
 
