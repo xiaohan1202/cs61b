8e83d92011c32a8289d24b749af9ab8136eba4fb
Xiaohan <962479661@qq.com>
2021-11-28 21:59:24 +0800

    hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,71 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private Boolean[][] grid;
+    private int opennum = 0;
+    private int N;
+    WeightedQuickUnionUF wqu;
+    public Percolation(int N) {
+        this.N = N;
+        wqu = new WeightedQuickUnionUF(N * N + 2);
+        grid = new Boolean[N][N];
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                grid[i][j] = false;
+            }
+        }
+        for (int i = 0; i < N; i++) {
+            wqu.union(0, xyTo1D(0, i));
+            wqu.union(N * N + 1, xyTo1D(N - 1, i));
+        }
+    }
+    // create N-by-N grid, with all sites initially blocked
+    private int xyTo1D (int x, int y){
+        return N * y + x;
+    }
+    public void open(int row, int col) {
+        grid[row][col] = true;
+        unionSite(row, col);
+        opennum += 1;
+    }
+    private void unionSite(int row, int col){
+        if(row != N - 1){
+            if(isOpen(row + 1, col)){
+                wqu.union(xyTo1D(row, col), xyTo1D(row + 1, col));
+            }
+        }
+       if(row != 0){
+           if(isOpen(row - 1, col)){
+               wqu.union(xyTo1D(row, col), xyTo1D(row - 1, col));
+           }
+       }
+       if(col != N - 1){
+           if(isOpen(row, col + 1)){
+               wqu.union(xyTo1D(row, col), xyTo1D(row, col + 1));
+           }
+       }
+       if(col != 0) {
+           if(isOpen(row, col - 1) && col != 0){
+               wqu.union(xyTo1D(row, col), xyTo1D(row, col - 1));
+           }
+       }
 
+    }
+    // open the site (row, col) if it is not open already
+    public boolean isOpen(int row, int col) {
+        return grid[row][col];
+    } // is the site (row, col) open?
+    public boolean isFull(int row, int col) {
+        return wqu.connected(0, xyTo1D(row, col)) && isOpen(row, col);
+    }
+    // is the site (row, col) full?
+    public int numberOfOpenSites() {
+        return opennum;
+    }
+    // number of open sites
+    public boolean percolates() {
+        return wqu.connected(0, N * N + 1);
+    }
+    // does the system percolate?
+//    public static void main(String[] args)   // use for unit testing (not required)
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,44 @@
 package hw2;
 
 public class PercolationStats {
-
+    int T;
+    int N;
+    Percolation[] perc = new Percolation[T];
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        this.T = T;
+        this.N = N;
+        for(int i = 0; i < T; i++) {
+            perc[i] = pf.make(N);
+        }
+    }
+    // perform T independent experiments on an N-by-N grid
+    public double mean() {
+        double sum = 0.0;
+        for(int i = 0; i < T; i++){
+            if(perc[i].percolates()){
+                sum += perc[i].numberOfOpenSites() * 1.0 / N * N;
+            }
+        }
+        return sum / T;
+    }
+    // sample mean of percolation threshold
+    public double stddev() {
+        double mean = mean();
+        double sum = 0.0;
+        for (int i = 0; i < T; i++){
+            if(perc[i].percolates()){
+                sum += Math.pow(perc[i].numberOfOpenSites() * 1.0 / N * N - mean, 2);
+            }
+        }
+        return sum / (T - 1);
+    }
+    // sample standard deviation of percolation threshold
+    public double confidenceLow() {
+        return mean() - 1.96 * Math.sqrt(stddev()) / Math.sqrt(T);
+    }
+    // low endpoint of 95% confidence interval
+    public double confidenceHigh() {
+        return mean() + 1.96 * Math.sqrt(stddev()) / Math.sqrt(T);
+    }
+    // high endpoint of 95% confidence interval
 }
