dd75f6c520faf4a74bad41278fcd2df66162b5e7
Xiaohan <962479661@qq.com>
2021-12-23 14:19:53 +0800

    lab11

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,29 +1,56 @@
 package lab11.graphs;
 
+import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  *  @author Josh Hug
  */
 public class MazeBreadthFirstPaths extends MazeExplorer {
-    /* Inherits public fields:
-    public int[] distTo;
-    public int[] edgeTo;
-    public boolean[] marked;
-    */
+
+//    public int[] distTo;
+//    public int[] edgeTo;
+//    public boolean[] marked;
+
+    private int s;
+    private int t;
+    private Maze maze;
 
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
-        // Add more variables here!
+        maze = m;
+        s = maze.xyTo1D(sourceX, sourceY);
+        t = maze.xyTo1D(targetX, targetY);
+        distTo[s] = 0;
+        edgeTo[s] = s;// Add more variables here!
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
     private void bfs() {
+        Queue<Integer> fringe = new LinkedList<>();
+        fringe.offer(s);
+        marked[s] = true;
+        announce();
+        while(!fringe.isEmpty() && fringe.peek() != t) {
+            int v = fringe.poll();
+            for(int w : maze.adj(v)) {
+                if (marked[w] == false){
+                    marked[w] = true;
+                    fringe.offer(w);
+                    edgeTo[w] = v;
+                    distTo[w] = distTo[v] + 1;
+                    announce();
+                }
+            }
+        }
         // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
     }
 
 
     @Override
     public void solve() {
-        // bfs();
+         bfs();
     }
 }
 
--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,5 +1,7 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.Stack;
+
 /**
  *  @author Josh Hug
  */
@@ -9,6 +11,14 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private class Node {
+        int v;
+        int parent;
+        public Node(int v, int parent){
+            this.v = v;
+            this.parent = parent;
+        }
+    }
 
     public MazeCycles(Maze m) {
         super(m);
@@ -16,7 +26,44 @@ public class MazeCycles extends MazeExplorer {
 
     @Override
     public void solve() {
+        dfs(new Node(0, 0));
+        while(!stack.isEmpty()) {
+            int v = stack.pop();
+            if(stack.isEmpty()) break;
+            edgeTo[v] = stack.peek();
+            announce();
+        }
         // TODO: Your code here!
+    }
+    private boolean cycle = false;
+    private boolean overlap;
+    private int overlapPoint;
+    private Stack<Integer> stack = new Stack<>();
+    private void dfs(Node node) {
+        marked[node.v] = true;
+        announce();
+        for (int w : maze.adj(node.v)) {
+            if(marked[w] && w != node.parent) {
+                cycle = true;
+                stack.push(w);
+                overlap = false;
+                overlapPoint = w;
+                return;
+            }
+            if(!marked[w]) {
+                marked[w] = true;
+                dfs(new Node(w, node.v));
+            }
+            if(cycle) {
+                if(overlap == false) {
+                    stack.push(w);
+                }
+                if(overlapPoint == w) {
+                    overlap = true;
+                }
+                return;
+            }
+        }
     }
 
     // Helper methods go here
